a402c5c74e74682e605943ffaaecb72d9f962780
satyap84 <satyaprakash84@gmail.com>
2023-11-22 21:25:23 +0530

    Revert "added a new function su This reverts commit b4df5b30beccbe7544c7803024f21ad5bba623a7.

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,1 @@
 print("adding all the data processing code related to fraud detection")
-
-a = 5
-b = 7
-def sum(a,b):
-    c=a+b
-    return b
-
-print ("added a new function sum which is not correct")
